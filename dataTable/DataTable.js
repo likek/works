48a9d1b1f10861e211f5e2c58597c12707f01552
likeke <likek@outlook.com>
2017-09-06 01:47:36 +0800

    数据表格插件修改

--- dataTable/DataTable.js
+++ dataTable/DataTable.js
@@ -4,6 +4,8 @@
 function DataTable(data, columns) {
     this.data = [].concat(data);
     this.columns = columns;
+    this.filterBy = null;
+    this.dataSort = null;
     this.thead = [];
     this.tfoot = [];
     this.table = document.createElement('table');
@@ -21,6 +23,38 @@ DataTable.prototype.tools = {
                 ele.style[item] = css[item];
             }
         }
+    },
+    JSONSort: function (json, a, b) {
+        var newJson = (function temp(arr) {
+            var i = arr.lastIndexOf(null);
+            if (i == -1) {
+                return arr;
+            }
+            arr.splice(i, 1);
+            return temp(arr);
+        })(json);
+
+        function comp(a, b) {
+            return new Function("a", "b", "return  " + a + ".toString().charCodeAt() -  " + b + ".toString().charCodeAt();");
+        }
+
+        return newJson.sort(comp(a, b));
+    },
+    filter: function (filterList,data) {
+        var _t = this;
+        var newData = [].concat(data);
+        for (var item in filterList) {
+            if (filterList.hasOwnProperty(item)) {
+                for (var i = 0, l = newData.length; i < l; i++) {
+                    if (filterList[item].indexOf(newData[i][item]) != -1) {
+                        newData.splice(i,1);
+                        i--;
+                        l--;
+                    }
+                }
+            }
+        }
+        return newData;
     }
 };
 DataTable.prototype.draw = function (container) {
@@ -29,6 +63,7 @@ DataTable.prototype.draw = function (container) {
     var css = _t.tools.css;
     var table = _t.table;
     var row = null, cell = null;
+    _t.data = _t.dataFormat(_t.data);
     //头
     var thead = null;
     if (_t.thead.length != 0) {
@@ -41,12 +76,15 @@ DataTable.prototype.draw = function (container) {
             _t.oncelldraw(cell, _t.thead[i]);
         }
         thead.appendChild(row);
-        _t.onrowdraw(row, _t.thead);
+        // _t.onrowdraw(row, _t.thead);
     }
     //体
     var tbody = document.createElement('tbody');
     var cellData = [];
     for (i = 0, l = _t.data.length; i < l; i++) {
+        if (_t.data[i] == null) {
+            continue;
+        }
         row = document.createElement('tr');
         for (var j = 0, le = _t.columns.length; j < le; j++) {
             cell = document.createElement('td');
@@ -83,35 +121,59 @@ DataTable.prototype.draw = function (container) {
     _t.ontabledraw(table, _t.data);
     container.appendChild(table);
 };
-DataTable.prototype.refreshData = function (data) {
+
+DataTable.prototype.dataFormat =function (data) {
+    var _t = this;
+    var filter = _t.tools.filter;
+    var JSONSort = _t.tools.JSONSort;
+    var newData = [].concat(data);
+    if(_t.filterBy){
+        newData = filter(_t.filterBy,newData);
+    }
+    if (_t.dataSort) {
+        newData = JSONSort(newData, "a." + _t.dataSort, "b." + _t.dataSort);
+    }
+    return newData;
+};
+
+
+DataTable.prototype.refreshData = function (newData) {
     var _t = this;
     var css = _t.tools.css;
+    var data = _t.dataFormat(newData);
+    var compLength = data.length - _t.data.length;
+    console.log(data.length+"---"+_t.data.length);
+    var colLength = _t.columns.length;
     var tbody = _t.table.querySelector('tbody');
-    var rows = tbody.querySelectorAll('tr'), row = null, cells = null, cell = null, cellData = null;
-    for (var i = 0, l = data.length; i < l; i++) {
-        row = rows[i]||document.createElement('tr');
-        cells = row.querySelectorAll('td');
-        for (var j = 0, le = _t.columns.length; j < le; j++) {
-            cell = cells[j]||document.createElement('td');
-            cellData = data[i][_t.columns[j].name] || _t.columns[j].default;
-            cell.innerHTML = cellData;
-            if(l>_t.data.length){
+    var rows = tbody.querySelectorAll('tr'), row = null, cells = null, cell = null, cellData = null,l=0,le=0;
+    if(compLength>0){
+        for(var i=0;i<compLength;i++){
+            row = document.createElement('tr');
+            for(var j=0;j<colLength;j++){
+                cell = document.createElement('td');
                 row.appendChild(cell);
             }
-            _t.oncelldraw(cell, cellData);
-            _t.columns[j].otherCss && css(cell, _t.columns[j].otherCss);
-        }
-        if(l>_t.data.length){
             tbody.appendChild(row);
         }
-        _t.onrowdraw(row, data[i]);
     }
-    if(data.length<_t.data.length){
-        var compLength = _t.data.length - data.length;
-        for(i = 1;i<=compLength;i++){
-            row = tbody.getElementsByTagName('tr')[_t.data.length-i];
-            row && tbody.removeChild(row);
+    if(compLength<0){
+        for(i = 0;i<Math.abs(compLength);i++){
+            tbody.removeChild(rows[_t.data.length - i - 1]);
         }
     }
-    _t.data = data;
+    _t.data = [].concat(data);
+    _t.data = _t.dataFormat(_t.data);
+    rows = tbody.querySelectorAll('tr');
+    for (i = 0, l = _t.data.length; i < l; i++) {
+        row = rows[i];
+        cells = row.querySelectorAll('td');
+        for (j = 0, le = _t.columns.length; j < le; j++) {
+            cell = cells[j];
+            cellData = _t.data[i][_t.columns[j].name] || _t.columns[j].default;
+            cell.innerHTML = cellData;
+            _t.columns[j].otherCss && css(cell, _t.columns[j].otherCss);
+            _t.oncelldraw(cell, cellData);
+        }
+        _t.onrowdraw(row, _t.data[i]);
+    }
 };
